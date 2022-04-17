4f37efa7bebbd8ffff1f2b96c10c82cace95b086
TJ <web3hrive@protonmail.com>
2022-04-16 20:37:37 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import React, { useEffect, useState } from "react";
+import { Layout } from "../Components";
+import "../styles/globals.scss";
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./pages/*.{html,js,jsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
