4b2f088df53ad1612a757ac057dbd2770b716320
TJ <web3hrive@protonmail.com>
2022-04-17 20:51:20 -0400

    Added PostDetail pages with page amenities; Added react-image loading; Added Author, Comments and CommentForm components.

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,12 +1,17 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
-const categories = [
-  { name: "Web 3.0 News", slug: "news" },
-  { name: "Analysis", slug: "analysis" },
-];
+import { getCategories } from "../services";
 
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
@@ -18,8 +23,8 @@ const Header = () => {
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
-          {categories.map((category) => (
-            <Link key={category.slug} href={"/category/${category.slug}"}>
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}>
               <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
                 {category.name}
               </span>
