8cd0c183d7b9ce75958dbcc4ef61174098bb8a6e
TJ <web3hrive@protonmail.com>
2022-04-18 01:04:45 -0400

    Completed content rendering for PostDetails. Completed Logo design.

--- Components/Author.jsx
+++ Components/Author.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import Image from "next/image";
 
-const Author = () => {
+import { grpahCMSImageLoader } from "../util";
+
+const Author = ({ author }) => {
   return (
-    <div>
-      <h1>Author</h1>
+    <div className="text-center mt-20 mb-8 p-8 relative rounded-lg bg-black bg-opacity-20">
+      <div className="absolute m-auto left-0 right-0 flex flex-row justify-center items-center -top-8 h-16 w-48 bg-white rounded-full">
+        <Image
+          unoptimized
+          loader={grpahCMSImageLoader}
+          alt={author.name}
+          height="150px"
+          width="150px"
+          className="align-middle object-contain"
+          src={author.photo.url}
+        />
+      </div>
+      <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-lg">{author.bio}</p>
     </div>
   );
 };
